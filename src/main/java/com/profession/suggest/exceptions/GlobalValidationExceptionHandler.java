4339278ca20b6bcf4bd9ad51bee6343db024a8de
lubuntum <lubuntum@gmail.com>
2025-12-05 04:27:44 +0700

    хендлер для валидации данных DTO

---
+++ src/main/java/com/profession/suggest/exceptions/GlobalValidationExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.profession.suggest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalValidationExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.BAD_REQUEST.value());
+
+        Map<String, String> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(e ->
+                fieldErrors.put(e.getField(), e.getDefaultMessage()));
+
+        errors.put("errors", fieldErrors);
+        return errors;
+    }
+}
