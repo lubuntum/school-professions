a706a2559947acd45c993bcfc9baab20ee161265
lubuntum <lubuntum@gmail.com>
2025-12-05 04:26:03 +0700

    Мапперы для прарсинга DTO в сущности hyb и обратно 2

---
+++ src/main/java/com/profession/suggest/dto/dataanalys/psychtests/PsychTestPupilRequestMapper.java
@@ -0,0 +1,31 @@
+package com.profession.suggest.dto.dataanalys.psychtests;
+
+import com.profession.suggest.database.entities.dataanalys.psychtests.PsychParam;
+import com.profession.suggest.database.entities.dataanalys.psychtests.PsychTest;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class PsychTestPupilRequestMapper {
+    private final PsychParamMapper psychParamMapper;
+
+    public PsychTestPupilRequestMapper(PsychParamMapper psychParamMapper) {
+        this.psychParamMapper = psychParamMapper;
+    }
+
+    public PsychTest fromDTO(PsychTestPupilRequestDTO dto) {
+        PsychTest psychTest = new PsychTest();
+        psychTest.setCompletionTimeSeconds(dto.getCompletionTimeSeconds());
+        psychTest.setPsychParams(
+                dto.getPsychParams().stream()
+                        .map(psychParamMapper::fromDTO)
+                        .collect(Collectors.toSet()));
+        return psychTest;
+
+    }
+
+}
