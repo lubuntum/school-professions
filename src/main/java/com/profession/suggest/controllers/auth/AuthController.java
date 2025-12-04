516caee23968e59e550eb42250e8584c7158df2c
lubuntum <lubuntum@gmail.com>
2025-12-05 04:26:25 +0700

    Контроллер для регистрации пока что автоматической через API

---
+++ src/main/java/com/profession/suggest/controllers/auth/AuthController.java
@@ -0,0 +1,27 @@
+package com.profession.suggest.controllers.auth;
+
+import com.profession.suggest.database.entities.pupil.Pupil;
+import com.profession.suggest.database.services.pupil.PupilService;
+import com.profession.suggest.dto.auth.AccountApiRegisterDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final PupilService pupilService;
+
+    public AuthController(PupilService pupilService) {
+        this.pupilService = pupilService;
+    }
+
+    @PostMapping("/auto-register")
+    public ResponseEntity<Pupil> autoRegister(@Valid @RequestBody AccountApiRegisterDTO accountApiRegisterDTO) {
+        return ResponseEntity.ok(pupilService.createWithAccount(accountApiRegisterDTO));
+    }
+
+}
