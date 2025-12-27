b7b3028eb330b7aa3dd6007385d6069b18137ff3
lubuntum <lubuntum@gmail.com>
2025-12-27 10:28:05 +0700

    получение данных отдельного студента по ключу контроллер

--- src/main/java/com/profession/suggest/controllers/pupil/PupilController.java
+++ src/main/java/com/profession/suggest/controllers/pupil/PupilController.java
@@ -3,19 +3,22 @@ package com.profession.suggest.controllers.pupil;
 import com.profession.suggest.database.services.pupil.PupilService;
 import com.profession.suggest.dto.pupil.PupilDTO;
 import com.profession.suggest.dto.pupil.PupilResponseDTO;
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.osgi.annotation.bundle.Header;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Pageable;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+/**
+ * TODO
+ * 1. get pupil values by token +
+ * 2. get all pupil tests data by token
+ * */
 @RestController
 @RequestMapping("/api/pupils")
 public class PupilController {
@@ -26,7 +29,7 @@ public class PupilController {
         this.pupilService = pupilService;
     }
 
-    @GetMapping("/get")
+    @GetMapping()
     public ResponseEntity<Page<PupilResponseDTO>> getAllValidPupilWithAccounts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
@@ -36,5 +39,9 @@ public class PupilController {
             @RequestParam(required = false) String email) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortDir.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC, sortBy));
     return ResponseEntity.ok(pupilService.getPupilsData(pageable));
+    }
+    @GetMapping("/pupil-by-id")
+    public ResponseEntity<PupilResponseDTO> getPupilData(@RequestAttribute("id") Long id) {
+        return ResponseEntity.ok(pupilService.getPupilData(id));
     }
 }
