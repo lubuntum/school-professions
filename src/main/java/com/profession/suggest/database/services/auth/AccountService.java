6bf8075f239770203c643c69c69305846cf28f7c
lubuntum <lubuntum@gmail.com>
2025-12-27 10:30:48 +0700

    Реализация регистрации, проверка почты + создания аккаунта ( без студента), логин поиск по почте + проверка хеша пароля, dto класс для работы с авторизацией и аут

--- src/main/java/com/profession/suggest/database/services/auth/AccountService.java
+++ src/main/java/com/profession/suggest/database/services/auth/AccountService.java
@@ -1,17 +1,62 @@
 package com.profession.suggest.database.services.auth;
 
 import com.profession.suggest.database.entities.auth.Account;
+import com.profession.suggest.database.entities.auth.role.Role;
+import com.profession.suggest.database.entities.auth.role.RoleEnum;
 import com.profession.suggest.database.repositories.auth.AccountRepository;
+import com.profession.suggest.database.services.auth.role.RoleService;
+import com.profession.suggest.dto.auth.AccountDTO;
+import com.profession.suggest.dto.auth.AccountRegisterRequestDTO;
+import com.profession.suggest.services.jwt.JWTService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.util.HashSet;
 
 @Service
 public class AccountService {
     private final AccountRepository repository;
-
-    public AccountService(AccountRepository repository) {
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
+    public AccountService(AccountRepository repository, RoleService roleService, PasswordEncoder passwordEncoder, JWTService jwtService) {
         this.repository = repository;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
-    public void createAccount(Account account) {
-        repository.save(account);
+    public Account createAccount(Account account) {
+        return repository.save(account);
+    }
+    public Boolean isEmailFree(String email) {
+        return repository.findByEmail(email) == null;
+    }
+    public String login (AccountDTO accountDTO) throws AccountNotFoundException {
+        Account account = repository.findByEmail(accountDTO.getEmail());
+        if (account == null)
+            throw new AccountNotFoundException("Email or password incorrect");
+        if (!passwordEncoder.matches(accountDTO.getPassword(), account.getPassword()))
+            throw new AccountNotFoundException("Email or password incorrect");
+
+        return jwtService.generateToken(String.valueOf(account.getId()));
+    }
+    @Transactional
+    public String registration(AccountRegisterRequestDTO accountRegisterRequestDTO) {
+        if (accountRegisterRequestDTO == null || accountRegisterRequestDTO.getEmail() == null
+                || accountRegisterRequestDTO.getPassword() == null)
+            throw new IllegalArgumentException("Fill all fields");
+        if (!isEmailFree(accountRegisterRequestDTO.getEmail()))
+            throw new IllegalArgumentException("Email already in use");
+        Account account = new Account();
+        account.setEmail(accountRegisterRequestDTO.getEmail());
+        account.setPassword(passwordEncoder.encode(accountRegisterRequestDTO.getPassword()));
+
+        Role role = roleService.findByName(RoleEnum.PUPIL);
+        if (account.getRoles() == null)
+            account.setRoles(new HashSet<>());
+        account.getRoles().add(role);
+        return repository.save(account).getEmail();
     }
 }
---
+++ src/main/java/com/profession/suggest/dto/auth/AccountDTO.java
@@ -0,0 +1,21 @@
+package com.profession.suggest.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AccountDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Size(max = 50, message = "Email cannot exceed 50 characters")
+    private String email;
+    @NotBlank(message = "password is required")
+    @Size(min = 8, max = 100, message = "password must be 8-100 characters")
+    private String password;
+}
