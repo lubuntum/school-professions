300bd2791989504fb87e3bb4ff0a9b26e7e2372c
lubuntum <lubuntum@gmail.com>
2026-02-05 06:38:49 +0700

    Бизнес логика для добавления оценок и доп инфы

--- src/main/java/com/profession/suggest/controllers/pupil/PupilSubjectsController.java
+++ src/main/java/com/profession/suggest/controllers/pupil/PupilSubjectsController.java
@@ -1,6 +1,9 @@
 package com.profession.suggest.controllers.pupil;
 
+import com.profession.suggest.database.services.pupil.PupilService;
 import com.profession.suggest.database.services.pupil.subject.PupilGradeService;
+import com.profession.suggest.database.services.pupil.subject.SubjectService;
+import com.profession.suggest.dto.pupil.subject.profile.PupilSubjectProfileDTO;
 import com.profession.suggest.dto.pupil.subject.PupilSubjectDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,18 +14,32 @@ import java.util.List;
 @RequestMapping("/api/pupil-subjects")
 public class PupilSubjectsController {
     private final PupilGradeService pupilGradeService;
+    private final PupilService pupilService;
+    private final SubjectService subjectService;
 
-    public PupilSubjectsController(PupilGradeService pupilGradeService) {
+    public PupilSubjectsController(PupilGradeService pupilGradeService, PupilService pupilService, SubjectService subjectService) {
         this.pupilGradeService = pupilGradeService;
+        this.pupilService = pupilService;
+        this.subjectService = subjectService;
     }
 
-    @PostMapping("/add-grades")
+    @PostMapping("/add-info")
     public ResponseEntity<String> addGradesToPupil(@RequestAttribute("accountId") Long accountId, @RequestBody List<PupilSubjectDTO> pupilSubjectDTOS) {
+        subjectService.addSubjectProfilesForPupil(pupilSubjectDTOS, pupilService.getPupilByAccountId(accountId));
         pupilGradeService.addGradesToPupil(accountId, pupilSubjectDTOS);
         return ResponseEntity.ok("Saved");
     }
     @GetMapping
     public ResponseEntity<List<PupilSubjectDTO>> getPupilSubjectWithGrades(@RequestAttribute("accountId") Long accountId) {
         return ResponseEntity.ok(pupilGradeService.getPupilSubject(accountId));
+    }
+    @PostMapping("/add-levels")
+    public ResponseEntity<String> addLevelsToPupil(@RequestAttribute("accountId") Long accountId, @RequestBody PupilSubjectProfileDTO pupilSubjectProfileDTO) {
+        try {
+            //TODO
+            return ResponseEntity.ok("Levels Saved");
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
--- src/main/java/com/profession/suggest/database/services/pupil/subject/SubjectService.java
+++ src/main/java/com/profession/suggest/database/services/pupil/subject/SubjectService.java
@@ -1,22 +1,34 @@
 package com.profession.suggest.database.services.pupil.subject;
 
+import com.profession.suggest.database.entities.pupil.Pupil;
 import com.profession.suggest.database.entities.pupil.subject.Subject;
 import com.profession.suggest.database.repositories.pupil.subject.SubjectRepository;
+import com.profession.suggest.database.services.pupil.subject.profile.PupilSubjectProfileService;
+import com.profession.suggest.dto.pupil.subject.PupilSubjectDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class SubjectService {
     private final SubjectRepository repository;
-
-    public SubjectService(SubjectRepository repository) {
+    private final PupilSubjectProfileService pupilSubjectProfileService;
+    public SubjectService(SubjectRepository repository, PupilSubjectProfileService pupilSubjectProfileService) {
         this.repository = repository;
+        this.pupilSubjectProfileService = pupilSubjectProfileService;
     }
     public Subject getSubjectByName(String name) {
         return repository.findByName(name);
     }
     public List<Subject> getAllSubjects() {
         return repository.findAll();
+    }
+    @Transactional
+    public void addSubjectProfilesForPupil(List<PupilSubjectDTO> pupilSubjectDTOS, Pupil pupil) {
+        pupilSubjectDTOS.forEach((pupilSubjectDTO -> {
+            Subject subject = repository.findByName(pupilSubjectDTO.getName());
+            pupilSubjectProfileService.createPupilSubjectProfile(pupil, subject, pupilSubjectDTO.getPupilSubjectProfileDTO());
+        }));
     }
 }
