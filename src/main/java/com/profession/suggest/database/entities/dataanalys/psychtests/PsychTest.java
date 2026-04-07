6dc1dba1f194e68246ef8cc081db31c41115427a
lubuntum <lubuntum@gmail.com>
2026-04-07 14:52:48 +0700

    При добавлении специалиста также добавил отношение в тест

--- src/main/java/com/profession/suggest/database/entities/dataanalys/psychtests/PsychTest.java
+++ src/main/java/com/profession/suggest/database/entities/dataanalys/psychtests/PsychTest.java
@@ -1,12 +1,12 @@
 package com.profession.suggest.database.entities.dataanalys.psychtests;
 
 import com.profession.suggest.database.entities.pupil.Pupil;
+import com.profession.suggest.database.entities.specialist.Specialist;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,10 +24,14 @@ public class PsychTest {
     private Double completionTimeSeconds;
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
-    //ManyToOne Pupil +
+    //Test completed by pupil
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "pupil_id")
     private Pupil pupil;
+    //Test completed by specialist
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "specialist_id")
+    private Specialist specialist;
     //manyToOne PsychTestType +
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "psych_test_type_id")
