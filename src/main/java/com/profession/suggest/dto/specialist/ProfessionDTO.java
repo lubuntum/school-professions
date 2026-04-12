9f2b1acb05985ae2edcbbf81063b5ba48f6ee91d
lubuntum <lubuntum@gmail.com>
2026-04-13 01:01:00 +0700

    dto для работы с профессиями

---
+++ src/main/java/com/profession/suggest/dto/specialist/ProfessionDTO.java
@@ -0,0 +1,12 @@
+package com.profession.suggest.dto.specialist;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfessionDTO {
+    private String name;
+}
