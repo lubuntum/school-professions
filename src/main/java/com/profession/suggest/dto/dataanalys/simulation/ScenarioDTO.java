408cd1d687fab3ef5a06e2ad5aac873fdfe59009
lubuntum <lubuntum@gmail.com>
2026-05-04 23:18:26 +0700

    Сценарии для симуляции

---
+++ src/main/java/com/profession/suggest/dto/dataanalys/simulation/ScenarioDTO.java
@@ -0,0 +1,13 @@
+package com.profession.suggest.dto.dataanalys.simulation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScenarioDTO {
+    private Long id;
+    private String name;
+}
