73d785482e3162e0ba81cfa4b2607d3cb781a777
lubuntum <lubuntum@gmail.com>
2026-05-19 13:16:04 +0700

    с inner join возвращаются только нужные PUpil или Specialist но не оба за раз (left join возвращал пустые значения)

--- src/main/java/com/profession/suggest/database/repositories/dataanalys/psychtests/PsychTestRepository.java
+++ src/main/java/com/profession/suggest/database/repositories/dataanalys/psychtests/PsychTestRepository.java
@@ -1,15 +1,12 @@
 package com.profession.suggest.database.repositories.dataanalys.psychtests;
 
-import com.profession.suggest.database.entities.auth.Account;
 import com.profession.suggest.database.entities.dataanalys.psychtests.PsychTest;
 import com.profession.suggest.database.entities.dataanalys.psychtests.PsychTestType;
-import com.profession.suggest.database.entities.pupil.Pupil;
+import com.profession.suggest.database.entities.users.pupil.Pupil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -29,16 +26,17 @@ public interface PsychTestRepository extends JpaRepository<PsychTest, Long> {
             "pt.psychTestType.name = :testTypeName")
     List<PsychTest> findByAccountIdAndTestType(@Param("accountId") Long accountId, @Param("testTypeName") String testTypeName);
     @Query("select distinct pt from PsychTest pt " +
-            "left join pt.pupil p " +
+            "inner join pt.pupil p " +
             "where pt.createdAt >= :startDate and pt.createdAt <= :endDate")
     List<PsychTest> findByPupilAndDateRange(@Param("startDate") LocalDateTime startDate,
                                             @Param("endDate") LocalDateTime endDate);
 
     @Query("select distinct pt from PsychTest pt " +
-            "left join pt.specialist s " +
+            "inner join pt.specialist s " +
             "where pt.createdAt >= :startDate and pt.createdAt <= :endDate")
     List<PsychTest> findBySpecialistAndDateRange(@Param("startDate") LocalDateTime startDate,
                                                  @Param("endDate") LocalDateTime endDate);
+
 
     @Query("select distinct pt from PsychTest pt " +
             "where pt.createdAt >= :startDate and pt.createdAt <= :endDate")
