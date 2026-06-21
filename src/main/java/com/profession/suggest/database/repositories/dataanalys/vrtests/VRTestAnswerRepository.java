4c9e3e989f5115266d4ea5ea3a24ba9e0ea83210
lubuntum <lubuntum@gmail.com>
2026-06-21 17:19:26 +0700

    entity и репозитории для VR тестов

---
+++ src/main/java/com/profession/suggest/database/repositories/dataanalys/vrtests/VRTestAnswerRepository.java
@@ -0,0 +1,7 @@
+package com.profession.suggest.database.repositories.dataanalys.vrtests;
+
+import com.profession.suggest.database.entities.dataanalys.vrtests.VRTestAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VRTestAnswerRepository extends JpaRepository<VRTestAnswer, Long> {
+}
