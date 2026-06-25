2d74aca141f45581c8f2d146f2bdcd6a2c7dad7c
lubuntum <lubuntum@gmail.com>
2026-06-26 05:22:07 +0700

    Получение всех специалистов компании (удобно для HR), получение компании для специалиста (просмотр в ЛК)

---
+++ src/main/java/com/profession/suggest/controllers/company/CompanyController.java
@@ -0,0 +1,52 @@
+package com.profession.suggest.controllers.company;
+
+import com.profession.suggest.database.services.auth.AccountService;
+import com.profession.suggest.database.services.specialist.CompanyService;
+import com.profession.suggest.dto.specialist.SpecialistDTO;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@RequestMapping("/api/company")
+public class CompanyController {
+    private final AccountService accountService;
+    private final CompanyService companyService;
+
+    @GetMapping()
+    public ResponseEntity<?> getMyCompany(@RequestAttribute("accountId") Long accountId) {
+        try {
+            return ResponseEntity.ok(companyService.getCompanyByAccountId(accountId));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        } catch (Exception e) {
+            log.error("Error getting company: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+    }
+    @GetMapping("/specialists")
+    public ResponseEntity<?> getCompanySpecialistsDTO(@RequestAttribute("accountId") Long accountId) {
+        try {
+            List<SpecialistDTO> specialists = companyService.getSpecialistDTOsByHRAccountId(accountId);
+            return ResponseEntity.ok(specialists);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        } catch (Exception e) {
+            log.error("Error getting company specialists: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+    }
+
+
+}
