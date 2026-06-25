ff5ff99caedcaa50cd0b677330e13b6e1cf8076b
lubuntum <lubuntum@gmail.com>
2026-06-26 05:22:55 +0700

    jwt токен продлено действие до 24 часов

--- src/main/java/com/profession/suggest/services/jwt/JWTService.java
+++ src/main/java/com/profession/suggest/services/jwt/JWTService.java
@@ -38,7 +38,7 @@ public class JWTService {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*10))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
                 .signWith(SignatureAlgorithm.HS256, env.getProperty(EnvPropertiesConfig.SECRET_KEY).getBytes())
                 .compact();
     }
