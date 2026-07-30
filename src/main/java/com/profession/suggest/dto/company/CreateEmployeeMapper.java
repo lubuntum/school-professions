ddffb810920bb8046be94140160ad5856cf88de5
lubuntum <lubuntum@gmail.com>
2026-07-30 16:27:08 +0700

    маппер для работников

--- src/main/java/com/profession/suggest/dto/company/CreateEmployeeMapper.java
+++ src/main/java/com/profession/suggest/dto/company/CreateEmployeeMapper.java
@@ -1,9 +1,13 @@
 package com.profession.suggest.dto.company;
 
 import com.profession.suggest.database.entities.auth.Account;
+import com.profession.suggest.database.entities.auth.role.Role;
+import com.profession.suggest.database.entities.auth.role.RoleEnum;
 import com.profession.suggest.database.entities.users.specialist.Company;
 import com.profession.suggest.database.entities.users.specialist.Specialist;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class CreateEmployeeMapper {
@@ -11,6 +15,8 @@ public class CreateEmployeeMapper {
         CreateEmployeeResponse response = new CreateEmployeeResponse();
         response.setEmail(account.getEmail());
         response.setAccountId(account.getId());
+        response.setRoles(account.getRoles().stream()
+                .map(Role::getName).collect(Collectors.toList()));
 
         Specialist specialist = account.getSpecialist();
         if (specialist == null) return response;
