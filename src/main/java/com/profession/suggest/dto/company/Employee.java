2d312c8225335b295aa9fce9f7604f00d806fa29
lubuntum <lubuntum@gmail.com>
2026-07-30 16:26:21 +0700

    Расширил класс Employee для выдачи ролей и доп информации как специалиста на предприятии

--- src/main/java/com/profession/suggest/dto/company/Employee.java
+++ src/main/java/com/profession/suggest/dto/company/Employee.java
@@ -1,9 +1,13 @@
 package com.profession.suggest.dto.company;
 
+import com.profession.suggest.database.entities.auth.role.RoleEnum;
+import com.profession.suggest.database.entities.gender.GenderEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -13,5 +17,12 @@ public class Employee {
     private Long id;
     private String fullName;
     private String email;
-    private String role;  // "HR" or "SPECIALIST"
+    private List<RoleEnum> roles;  // "HR" or "SPECIALIST"
+    //additional fields for HR data
+    private String contactEmail;
+    private String contactPhone;
+    private String experience;
+    private String jobSatisfaction;
+    private String profession;
+    private GenderEnum gender;
 }
